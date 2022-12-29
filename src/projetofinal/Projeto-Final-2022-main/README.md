18e6ea89268b7926f7c33b4b98770cb9c4b7548b
Anderson Kbral <102990299+Andinhoo-kiddo@users.noreply.github.com>
2022-12-29 01:08:24 -0300

    Add files via upload
    java eu te odeio

---
+++ src/projetofinal/Projeto-Final-2022-main/README.md
@@ -0,0 +1,2 @@
+# Projeto-Final-2022
+Projeto final do curso de informática do ano de 2022, matérias envolvidas: PROO &amp; BCDD
---
+++ src/projetofinal/Projeto-Final-2022-main/src/DTO/AvaliacaoDTO.java
@@ -0,0 +1,22 @@
+package DTO;
+public class AvaliacaoDTO {
+    private String estrela_avaliacao, opniao_avaliacao;
+
+    public String getEstrela_avaliacao() {
+        return estrela_avaliacao;
+    }
+
+    public void setEstrela_avaliacao(String estrela_avaliacao) {
+        this.estrela_avaliacao = estrela_avaliacao;
+    }
+
+    public String getOpniao_avaliacao() {
+        return opniao_avaliacao;
+    }
+
+    public void setOpniao_avaliacao(String opniao_avaliacao) {
+        this.opniao_avaliacao = opniao_avaliacao;
+    }
+    
+    
+}
